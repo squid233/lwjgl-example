feb294bbc187a7485feaf48cc64e5540f317bfbe
squid233 <60126026+squid233@users.noreply.github.com>
2021-10-02 15:27:03 +0800

    Fixed a bug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class Main implements Runnable, AutoCloseable {
     public GLFWindow window;
+    public Framebuffer framebuffer; 
 
     public static void main(String[] args) {
         try (Main main = new Main()) {
